498855b13c9cfe9155e282a6211c5f1de91e3735
India Hiring <indiahiring@global.publicisgroupe.net>
2019-09-21 11:23:16 +0530

    Add FootballLeageueService Repo

---
+++ src/main/java/com/football/league/footballLeagueService/FootballLeagueServiceApplication.java
@@ -0,0 +1,13 @@
+package com.football.league.footballLeagueService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballLeagueServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootballLeagueServiceApplication.class, args);
+	}
+
+}
